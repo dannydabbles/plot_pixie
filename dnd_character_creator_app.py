0f69b49a03e3c2b50321b6d40391747742001ef4
Danny Key <danny@dannydabbles.com>
2023-09-08 23:34:51 -0700

    Improving app

--- dnd_character_creator_app.py
+++ dnd_character_creator_app.py
@@ -1,102 +1,105 @@
 import os
 import openai
 import streamlit as st
-import boto3
-import asyncio
-from PIL import Image
+import pandas as pd
+from uuid import uuid4
 
-# Configuration from environment variables
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+# Set OpenAI API Key
+openai.api_key = os.environ.get('OpenAI_API_Key')
 
-openai.api_key = OPENAI_API_KEY
-s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
-
-st.set_page_config(
-    page_title="D&D Character Generator with Portraits",
-    layout="centered"
-)
-
-def openai_completion(prompt):
-    """Send a text prompt to ChatGPT and return its generated completion."""
-    response = openai.Completion.create(prompt=prompt, max_tokens=150, temperature=0.5)
-    return response['choices'][0]['text']
-
-async def generate_character_details(character):
-    """Use ChatGPT to fill in missing character details."""
-    if not character.get("description"):
-        prompt = f"Describe a character named {character['name']} who is a {character['race']}."
-        character['description'] = openai_completion(prompt)
-    return character
-
-async def generate_portrait(prompt):
-    """Send a prompt to DALL-E and return the generated image URL."""
-    response = openai.Image.create(prompt=prompt, n=1, size="256x256")
-    return response['data'][0]['url']
-
-def upload_to_s3(image_url):
-    """Upload a generated portrait to S3 and return its public URL."""
-    s3.upload_file(image_url, S3_BUCKET_NAME, 'path_in_bucket')
-    s3_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/path_in_bucket"
-    return s3_url
-
-def generate_character_sheet(character):
-    """Generate a HTML character sheet."""
-    return f"""
-    <div style="border:2px solid black;padding:20px;">
-        <h2>{character['name']}</h2>
-        <p><strong>Description:</strong> {character['description']}</p>
-        <p><strong>Race:</strong> {character['race']}</p>
-        <p><strong>Backstory:</strong> {character['backstory']}</p>
-        <p><strong>Alignment:</strong> {character['alignment']}</p>
-        <p><strong>Traits:</strong> {character['traits']}</p>
-        <p><strong>Ideals:</strong> {character['ideals']}</p>
-        <p><strong>Bonds:</strong> {character['bonds']}</p>
-        <p><strong>Flaws:</strong> {character['flaws']}</p>
-        <img src="{character['portrait_url']}" alt="Character Portrait" style="width:200px;height:200px;">
-    </div>
+def get_character_data(character):
     """
+    Query the ChatGPT API to fill out missing character data based on provided data.
+    """
+    prompt = f"Describe a D&D character with the following attributes: {character}"
+    response = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=150)
+    return response.choices[0].text.strip()
+
+def generate_portrait(prompt):
+    """
+    Generate a portrait based on the prompt using DALL-E and save it locally.
+    """
+    response = openai.Image.create(prompt=prompt, n=1, size="256x256")
+    image_url = response.data[0]['url']
+    filename = f"portraits/{uuid4()}.png"
+    st.image(image_url).save(filename)
+    return filename
+
+def generate_html_character_sheet(character, portrait_filenames):
+    """
+    Generate an HTML character sheet based on character data and save it locally.
+    """
+    html_content = f"""
+    <h1>{character['name']}</h1>
+    <p><strong>Race:</strong> {character['race']}</p>
+    <p><strong>Class:</strong> {character['class']}</p>
+    <p><strong>Alignment:</strong> {character['alignment']}</p>
+    <p><strong>Background:</strong> {character['background']}</p>
+    <p><strong>Description:</strong> {character['description']}</p>
+    <p><strong>Personality Traits:</strong> {character['personality_traits']}</p>
+    <p><strong>Ideals:</strong> {character['ideals']}</p>
+    <p><strong>Bonds:</strong> {character['bonds']}</p>
+    <p><strong>Flaws:</strong> {character['flaws']}</p>
+    """
+    for filename in portrait_filenames:
+        html_content += f'<img src="{filename}" alt="Portrait of {character["name"]}">'
+    filename = f"character_sheets/{uuid4()}.html"
+    with open(filename, 'w') as file:
+        file.write(html_content)
+    return filename
 
 def main():
-    """Main function for the Streamlit app."""
+    """
+    Main function for the Streamlit app.
+    """
     st.title("D&D Character Generator with Portraits")
 
-    # Form Fields
     character = {
-        "name": st.text_input("Name"),
-        "description": st.text_area("Description"),
-        "race": st.selectbox("Race", ["Elf", "Dwarf", "Human"]),
-        "backstory": st.text_area("Backstory"),
-        "alignment": st.radio("Alignment", ["Lawful Good", "Chaotic Neutral"]),
-        "traits": st.text_area("Personality Traits"),
+        "name": st.text_input("Character Name", "Aelar"),
+        "age": st.number_input("Age", min_value=1, max_value=500, value=25),
+        "race": st.selectbox("Race", ["Human", "Elf", "Dwarf", "Orc", "Tiefling", "Custom"]),
+        "class": st.selectbox("Class", ["Warrior", "Mage", "Rogue", "Cleric", "Ranger", "Custom"]),
+        "alignment": st.selectbox("Alignment", ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]),
+        "background": st.selectbox("Background", ["Acolyte", "Criminal", "Folk Hero", "Noble", "Sage", "Soldier", "Custom"]),
+        "personality_traits": st.text_area("Personality Traits"),
         "ideals": st.text_area("Ideals"),
         "bonds": st.text_area("Bonds"),
-        "flaws": st.text_area("Flaws")
+        "flaws": st.text_area("Flaws"),
+        "description": st.text_area("Description")
     }
 
-    portrait_prompt = st.text_area("Portrait Prompt (Optional. If empty, we'll generate one based on the description.)")
+    # Custom fields input
+    if character["race"] == "Custom":
+        character["race"] = st.text_input("Specify Your Custom Race")
+    if character["class"] == "Custom":
+        character["class"] = st.text_input("Specify Your Custom Class")
+    if character["background"] == "Custom":
+        character["background"] = st.text_input("Specify Your Custom Background")
 
-    # Generate and Display Character Sheet
+    # Number of portraits
+    num_portraits = st.slider("Number of Portraits", 1, 5)
+
     if st.button("Generate Character Sheet"):
-        try:
-            # Fill in missing details
-            character = asyncio.run(generate_character_details(character))
-            
-            # Get portrait
-            if not portrait_prompt:
-                portrait_prompt = f"A portrait of {character['name']}, a {character['description']}"
-            image_url = asyncio.run(generate_portrait(portrait_prompt))
-            s3_url = upload_to_s3(image_url)
-            character["portrait_url"] = s3_url
-            
-            # Display character sheet
-            html_sheet = generate_character_sheet(character)
-            st.markdown(html_sheet, unsafe_allow_html=True)
+        # Fetch filled out character data from the API
+        character["description"] = get_character_data(character)
 
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+        # Generate portrait prompts and portraits
+        portrait_filenames = []
+        for _ in range(num_portraits):
+            portrait_prompt = get_character_data({key: character[key] for key in ["name", "race", "class", "background"]})
+            portrait_filenames.append(generate_portrait(portrait_prompt))
 
-if __name__ == '__main__':
+        # Generate HTML character sheet and save
+        html_filename = generate_html_character_sheet(character, portrait_filenames)
+
+        # Save character data as parquet
+        df = pd.DataFrame([character])
+        parquet_filename = f"data/{uuid4()}.parquet"
+        df.to_parquet(parquet_filename)
+
+        # Redirect to the HTML character sheet
+        st.write(f"Character sheet saved at {html_filename} and data saved at {parquet_filename}")
+        st.markdown(f"[Click here to view the character sheet]({html_filename})")
+
+if __name__ == "__main__":
     main()
