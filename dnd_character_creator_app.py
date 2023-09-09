aeb68dfba619468d2ededd26b14d0af7a148889f
Danny Key <danny@dannydabbles.com>
2023-09-08 22:52:35 -0700

    First draft of DnD Character Creator App

--- dnd_character_creator_app.py
+++ dnd_character_creator_app.py
@@ -1,38 +1,82 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import os
+import openai
 import streamlit as st
+import boto3
+import asyncio
+from PIL import Image
 
-"""
-# Welcome to Streamlit!
+# Configuration from environment variables
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 
-Edit `/dnd_character_creator_app.py` to customize this app to your heart's desire :heart:
+openai.api_key = OPENAI_API_KEY
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.set_page_config(
+    page_title="D&D Character Generator with Portraits",
+    layout="centered"
+)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def openai_completion(prompt):
+    """Send a text prompt to ChatGPT and return its generated completion."""
+    response = openai.Completion.create(prompt=prompt, max_tokens=150, temperature=0.5)
+    return response['choices'][0]['text']
 
+async def generate_character_details(character):
+    """Use ChatGPT to fill in missing character details."""
+    if not character.get("description"):
+        prompt = f"Describe a character named {character['name']} who is a {character['race']}."
+        character['description'] = openai_completion(prompt)
+    return character
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+async def generate_portrait(prompt):
+    """Send a prompt to DALL-E and return the generated image URL."""
+    response = openai.Image.create(prompt=prompt, n=1, size="256x256")
+    return response['data'][0]['url']
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+def upload_to_s3(image_url):
+    """Upload a generated portrait to S3 and return its public URL."""
+    s3.upload_file(image_url, S3_BUCKET_NAME, 'path_in_bucket')
+    s3_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/path_in_bucket"
+    return s3_url
 
-    points_per_turn = total_points / num_turns
+def main():
+    """Main function for the Streamlit app."""
+    st.title("D&D Character Generator with Portraits")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    # Form Fields
+    character = {
+        "name": st.text_input("Name"),
+        "description": st.text_area("Description"),
+        "race": st.selectbox("Race", ["Elf", "Dwarf", "Human"]),
+        "backstory": st.text_area("Backstory"),
+        "alignment": st.radio("Alignment", ["Lawful Good", "Chaotic Neutral"]),
+        "traits": st.text_area("Personality Traits"),
+        "ideals": st.text_area("Ideals"),
+        "bonds": st.text_area("Bonds"),
+        "flaws": st.text_area("Flaws")
+    }
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Generate and Display Character Sheet
+    if st.button("Generate Character Sheet"):
+        try:
+            character = asyncio.run(generate_character_details(character))
+            st.write(character)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+
+    # Portrait Generation
+    portrait_prompt = st.text_area("Portrait Prompt")
+    if portrait_prompt and st.button("Generate Portraits"):
+        try:
+            image_url = asyncio.run(generate_portrait(portrait_prompt))
+            st.image(image_url)
+            s3_url = upload_to_s3(image_url)
+            st.write(f"Image saved at: {s3_url}")
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+
+if __name__ == '__main__':
+    main()
